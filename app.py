1879283d1a21e34969943cac9b47c15657a7099c
JoseJCanedo <canedo_jose@columbusstate.edu>
2025-02-03 13:55:56 +0000

    flask app

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template     #import flask framework
+import requests
+app = Flask(__name__)       #create app instance
+
+@app.route("/")             #decorator defines routes in app, in this case home '/'
+def home():
+    URL = "https://xkcd.com/info.0.json"
+    req = requests.get(url = URL)
+    data = req.json()
+    return render_template("index.html", data = data)
+
+@app.route("/pastComic")             #decorator defines routes in app, in this case home '/'
+def pastComic():
+    URL = "https://xkcd.com/info.0.json"
+    req = requests.get(url = URL)
+    data = req.json()
+    return render_template("index.html", data = data)
+
+@app.route("/who/<name>/who")  #second route with query parameter called name
+def hello_pathParam(name):
+    return {"name": name}
+
+if __name__=="__main__":
+    app.run(debug=True)
+
+# 127.0.0.1:5000
+# path parameter/variable Part of the URL
+# query parameter/variable Not a part of the URL necessary for navigation /who?name=jose
