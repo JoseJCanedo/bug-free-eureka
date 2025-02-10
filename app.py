7104e526c1b67da680988753a24934c403f3ab4c
JoseJCanedo <canedo_jose@columbusstate.edu>
2025-02-10 15:19:08 +0000

    s

--- app.py
+++ app.py
@@ -1,23 +1,39 @@
-from flask import Flask, render_template     #import flask framework
+from flask import Flask, render_template  # import flask framework
 import requests
-app = Flask(__name__)       #create app instance
+import logging
+app = Flask(__name__)  # create app instance
 
-@app.route("/")             #decorator defines routes in app, in this case home '/'
+@app.route("/")  # decorator defines routes in app, in this case home '/'
 def home():
-    URL = "https://xkcd.com/info.0.json"
-    req = requests.get(url = URL)
-    data = req.json()
-    return render_template("index.html", data = data)
+    try:
+        URL = "https://xkcd.com/info.0.json"
+        response = requests.get(url=URL)
+        if (response.status_code != 200):
+            raise ValueError("Error connecting to XKCD please check back later.")
+        return render_template("index.html", data=response.json())
+    except ValueError as e:
+        logging.error(f"ValueError with home: {e}")
+        return render_template("errorPage.html", errorMessage = e)
 
 @app.route("/pastComic/<comicNum>")             #decorator defines routes in app, in this case home '/'
 def pastComic(comicNum):
-    if(comicNum.isdigit() and int(comicNum) <= 3000):
+    try:
+        logging.info(f"Past Comic URL with value: {comicNum}")
+        if not comicNum.isdigit():
+            raise ValueError(f"The requested comic should be a integer. Received '{comicNum}' instead")
+        if(int(comicNum) > 3000):
+            raise ValueError(f"No comic available for '{comicNum}'. Please try another number smaller than 3001")
         URL = "https://xkcd.com/" + comicNum + "/info.0.json"
-        req = requests.get(url = URL)
-        data = req.json()
-        return render_template("pastComic.html", data = data)
-    else:
-        return render_template("errorPage.html")
+        response = requests.get(url = URL)
+        if (response.status_code != 200):
+            raise ValueError("Error connecting to XKCD please check back later.")
+        return render_template("pastComic.html", data = response.json())
+    except ValueError as e:
+        logging.error(f"ValueError with pastComic: {e}")
+        return render_template("errorPage.html", errorMessage = e)
+    except Exception as e:
+        logging.error(f"Exception with pastComic: {e}")
+        return render_template("errorPage.html", errorMessage = e)
 
 @app.route("/who/<name>/who")  #second route with query parameter called name
 def hello_pathParam(name):
